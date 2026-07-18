93870242b9cffe2c94713004f02afc8da0fe16b5
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-07-18 00:09:45 -0500

    changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,7 +4,23 @@ async function getMembersData() {
     const response = await fetch("data/members.json");
     const data = await response.json();
     
-    console.table(data.members);
+    // console.table(data.members);
+    displayMembers(data.members);
 }
 
 getMembersData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        // img email phone url
+
+        let businessName = document.createElement('h3');
+        let logo = document.createElement('img');
+        let email = document.createElement('p');
+        let phone = document.createElement('p');
+        let url = document.createElement('p');
+
+        
+    });
+}
