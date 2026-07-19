d2a2c9c2739aa5575d58e6e954cbb1ca6e2ae8e7
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-07-18 23:03:45 -0500

    changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -29,10 +29,6 @@ const displayMembers = (members) => {
         logo.setAttribute('width', '80');
         logo.setAttribute('height', '80');
 
-
-
-
-
         address.textContent = `${member.address}`;
         phone.textContent = `${member.phone}`;
         website.textContent = `${member.website}`;
