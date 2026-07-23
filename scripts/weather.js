5a5c4ae7b650d73991fb1f9cdee05f886bed26d9
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-07-22 23:41:22 -0500

    changes

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,3 +1,5 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&units={metric}&appid={2fdc2d4b07b9aaaa089c967e73c9bb77}';
