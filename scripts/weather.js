2725105eebc75ff4dee599854650ddbb626265de
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-07-23 13:06:23 -0500

    changes

--- scripts/weather.js
+++ scripts/weather.js
@@ -3,3 +3,20 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&units={metric}&appid={2fdc2d4b07b9aaaa089c967e73c9bb77}';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
