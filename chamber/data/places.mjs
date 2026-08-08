7c03a64c1900932c605640e8cd06edbcef79421e
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-08-08 01:05:12 -0500

    changes

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -4,55 +4,55 @@ export const places = [
       "address": "Av. Angela Perotti, Huacachina, Ica 11000",
       "cost": "S/ 40 (about $13 USD)",
       "description": "Ride a dune buggy through the desert dunes and practice sandboarding. One of the most popular adventure activities in Ica.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Huacachina+Ica+Peru+sandboarding"
+      "photo_url": "huacachina.webp"
     },
     {
       "name": "Cachiche",
       "address": "Ica 11004, Peru",
       "cost": "Free",
       "description": "Explore the traditional Cachiche area, famous for its legends about witches and its distinctive local attractions.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Cachiche+Ica+Peru"
+      "photo_url": "cachiche.webp"
     },
     {
       "name": "Regional Museum of Ica Adolfo Bermudez Jenkins",
       "address": "Av. Ayabaca 895, Urb. San Isidro, Ica",
       "cost": "S/ 8 (about $2 USD)",
       "description": "Explore archaeological and historical collections from cultures including Paracas, Nasca, Wari, Ica, Chincha and Inca. The museum also has a bioarchaeology collection.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Museo+Regional+de+Ica+Adolfo+Bermudez+Jenkins"
+      "photo_url": "ica-museum.webp"
     },
     {
       "name": "Ballestas Islands",
       "address": "Muelle El Chaco, Paracas",
       "cost": "S/ 40 (about $13 USD)",
       "description": "Take a boat tour around the Ballestas Islands and see sea lions, seabirds, penguins and other marine wildlife.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Islas+Ballestas+Peru"
+      "photo_url": "ballestas-islands.webp"
     },
     {
       "name": "Paracas National Reserve",
       "address": "Paracas National Reserve, Ica, Peru",
       "cost": "S/ 50 (about $16 USD)",
       "description": "Explore the combination of desert and Pacific Ocean landscapes, including beaches, cliffs, viewpoints and wildlife.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Paracas+National+Reserve+Peru"
+      "photo_url": "reserva-nacional-paracas.webp"
     },
     {
       "name": "Cañon de los Perdidos",
       "address": "Cañon de los Perdidos, Ica, Peru",
       "cost": "S/ 60 (about $18 USD)",
       "description": "Visit a spectacular geological canyon in the Ica desert. It is a more adventurous option and usually requires transportation or an organized tour.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Canon+de+los+Perdidos+Ica+Peru"
+      "photo_url": "canon-of-perdidos.webp"
     },
     {
       "name": "Tatá Rooftop",
       "address": "Brunias L45, Ica 11001",
       "cost": "S/ 50 (about $16 USD)",
       "description": "A rooftop restaurant and gastropub suitable for dinner, socializing and enjoying an evening atmosphere.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Tata+Rooftop+Ica+Peru"
+      "photo_url": "tata-rooftop.webp"
     },
     {
       "name": "Local Peruvian and Ica Cuisine",
       "address": "Various restaurants throughout Ica",
       "cost": "S/ 30 (about $9 USD)",
       "description": "Try traditional Peruvian and regional Ica dishes at local restaurants and turn your visit into a food experience.",
-      "photo_url": "https://www.google.com/search?tbm=isch&q=Ica+Peru+traditional+food"
+      "photo_url": "restaurant-ica.webp"
     }
 ]
