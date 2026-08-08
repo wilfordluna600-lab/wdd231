7acc1241e04d71ef009e4a21e547b12affba43af
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-08-07 19:31:13 -0500

    changes

---
+++ chamber/data/places.mjs
@@ -0,0 +1,136 @@
+{
+  "destination": "Ica, Peru",
+  "activities": [
+    {
+      "id": 1,
+      "name": "Huacachina Dune Buggy and Sandboarding",
+      "location": "Huacachina Lagoon Reserved Zone, Ica, Peru",
+      "address": "Av. Angela Perotti, Huacachina, Ica 11000",
+      "cost": {
+        "currency": "PEN",
+        "amount": "40-105",
+        "type": "approximate"
+      },
+      "description": "Ride a dune buggy through the desert dunes and practice sandboarding. One of the most popular adventure activities in Ica.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Huacachina+Ica+Peru+sandboarding"
+    },
+    {
+      "id": 2,
+      "name": "Huacachina Sunset",
+      "location": "Huacachina Oasis, Ica, Peru",
+      "address": "Huacachina, Ica 11000",
+      "cost": {
+        "currency": "PEN",
+        "amount": 0,
+        "type": "free"
+      },
+      "description": "Climb one of the surrounding dunes and watch the sunset over the desert and Huacachina oasis.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Huacachina+sunset+Ica+Peru"
+    },
+    {
+      "id": 3,
+      "name": "Tacama Winery",
+      "location": "Tambo de Tacama, Ica, Peru",
+      "address": "Cam. Real 390, Ica 11004",
+      "cost": {
+        "currency": "PEN",
+        "amount": "45-145",
+        "type": "depends_on_tour"
+      },
+      "description": "Visit one of Ica's historic vineyards, learn about wine and pisco production, explore the traditional hacienda and participate in tastings.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Tacama+winery+Ica+Peru"
+    },
+    {
+      "id": 4,
+      "name": "Cachiche",
+      "location": "Cachiche, Ica, Peru",
+      "address": "Ica 11004, Peru",
+      "cost": {
+        "currency": "PEN",
+        "amount": 0,
+        "type": "free_or_low_cost"
+      },
+      "description": "Explore the traditional Cachiche area, famous for its legends about witches and its distinctive local attractions.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Cachiche+Ica+Peru"
+    },
+    {
+      "id": 5,
+      "name": "Regional Museum of Ica Adolfo Bermudez Jenkins",
+      "location": "Ica, Peru",
+      "address": "Av. Ayabaca 895, Urb. San Isidro, Ica",
+      "cost": {
+        "currency": "PEN",
+        "adult": 8,
+        "higher_education_student": 4,
+        "school_student": 1
+      },
+      "description": "Explore archaeological and historical collections from cultures including Paracas, Nasca, Wari, Ica, Chincha and Inca. The museum also has a bioarchaeology collection.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Museo+Regional+de+Ica+Adolfo+Bermudez+Jenkins"
+    },
+    {
+      "id": 6,
+      "name": "Ballestas Islands",
+      "location": "Paracas, Ica Region, Peru",
+      "address": "Muelle El Chaco, Paracas",
+      "cost": {
+        "currency": "PEN",
+        "amount": "40+",
+        "type": "approximate_tour_price"
+      },
+      "description": "Take a boat tour around the Ballestas Islands and see sea lions, seabirds, penguins and other marine wildlife.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Islas+Ballestas+Peru"
+    },
+    {
+      "id": 7,
+      "name": "Paracas National Reserve",
+      "location": "Paracas, Ica Region, Peru",
+      "address": "Paracas National Reserve, Ica, Peru",
+      "cost": {
+        "currency": "PEN",
+        "amount": "varies",
+        "type": "depends_on_entry_or_tour"
+      },
+      "description": "Explore the combination of desert and Pacific Ocean landscapes, including beaches, cliffs, viewpoints and wildlife.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Paracas+National+Reserve+Peru"
+    },
+    {
+      "id": 8,
+      "name": "Cañon de los Perdidos",
+      "location": "Ica Region, Peru",
+      "address": "Cañon de los Perdidos, Ica, Peru",
+      "cost": {
+        "currency": "PEN",
+        "amount": "50-150+",
+        "type": "approximate_tour_price"
+      },
+      "description": "Visit a spectacular geological canyon in the Ica desert. It is a more adventurous option and usually requires transportation or an organized tour.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Canon+de+los+Perdidos+Ica+Peru"
+    },
+    {
+      "id": 9,
+      "name": "Tatá Rooftop",
+      "location": "Ica, Peru",
+      "address": "Brunias L45, Ica 11001",
+      "cost": {
+        "currency": "PEN",
+        "amount": "40-140",
+        "type": "approximate_per_person"
+      },
+      "description": "A rooftop restaurant and gastropub suitable for dinner, socializing and enjoying an evening atmosphere.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Tata+Rooftop+Ica+Peru"
+    },
+    {
+      "id": 10,
+      "name": "Local Peruvian and Ica Cuisine",
+      "location": "Ica, Peru",
+      "address": "Various restaurants throughout Ica",
+      "cost": {
+        "currency": "PEN",
+        "amount": "30-100+",
+        "type": "approximate"
+      },
+      "description": "Try traditional Peruvian and regional Ica dishes at local restaurants and turn your visit into a food experience.",
+      "photo_url": "https://www.google.com/search?tbm=isch&q=Ica+Peru+traditional+food"
+    }
+  ]
+}
