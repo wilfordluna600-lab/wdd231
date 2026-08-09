e5fa767c73606bf02946ec6d1c5739194f142bf7
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-08-08 22:54:35 -0500

    changes

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -1,2 +1,26 @@
 import { places } from '../data/places.mjs'
 console.log(places)
+
+const showHere = document.querySelector("#allplaces")
+
+function displayItems(places) {
+    places.forEach(x => {
+        const thecard = document.createElement('div')
+        const thephoto = document.createElement('img')
+        thephoto.src = 'images/${x.photo_link}'
+        thephoto.alt = x.name
+        thecard.appendChild(thephoto)
+        const thetitle = document.createElement('h2')
+        thetitle.innerText = x.name
+        thecard.appendChild(thetitle)
+        const theaddress = document.createElement('address')
+        theaddress.innerText = x.address
+        thecard.appendChild(theaddress)
+        const thedesc = document.createElement('p')
+        thedesc.innerText = x.description
+        thecard.appendChild(thedesc)
+
+        showHere.appendChild(thecard)
+
+    })
+}
