5d670aa903a4d33ad24b4a691ec2e339e68adfbe
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-08-10 21:14:21 -0500

    changes

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -44,5 +44,15 @@ else {
     const difference = currentVisit - Number(lastVisit);
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
 
-    
+    if (days < 1) {
+        visitMessage.textContent = "Back so soon! Awesome!";
+    }
+    else if (days === 1) {
+        visitMessage.textContent = "You last visited 1 day ago.";
+    }
+    else {
+        visitMessage.textContent = `You last visited ${days} days ago.`;
+    }
 }
+
+localStorage.setItem("lastVisit", currentVisit);
