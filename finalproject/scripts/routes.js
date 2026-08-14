dc7b0f494f9eac369880451925fe93cf975e3dbb
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-08-14 17:43:38 -0500

    changes

---
+++ finalproject/scripts/routes.js
@@ -0,0 +1,30 @@
+import { routes } from '../data/routes.mjs';
+console.log(routes);
+
+
+const showHere = document.querySelector("#allroutes");
+
+function displayItems(routes) {
+    routes.forEach(x => {
+        const thecard = document.createElement('div');
+        const thephoto = document.createElement('img');
+        thephoto.src = `images/${x.photo_url}`;
+        thephoto.alt = x.name;
+        thephoto.loading = 'lazy';
+        thephoto.width = 300;
+        thephoto.height = 200;
+        thecard.appendChild(thephoto);
+        const thetitle = document.createElement('h2');
+        thetitle.innerText = x.name;
+        thecard.appendChild(thetitle);
+        const thedistance = document.createElement('p');
+        thedistance.innerText = x.distance;
+        thecard.appendChild(thedistance);
+        const thedesc = document.createElement('p');
+        thedesc.innerText = x.description;
+        thecard.appendChild(thedesc);
+        showHere.appendChild(thecard);
+    })
+}
+
+displayItems(routes);
